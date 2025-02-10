3a0da52e239139015cc708b1fd87608c37d353c1
Peerawit10 <wnowno1023@hotmail.com>
2025-02-10 15:22:16 +0700

    fix css

--- condiment.php
+++ condiment.php
@@ -18,7 +18,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>เครื่องปรุง - ร้านขายของชำ</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css?t=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
